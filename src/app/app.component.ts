f9a2aa7044c86067a1037aa4fc0bc99ebed0f60b
Mick Evans <codemastermick@gmail.com>
2021-04-28 14:52:41 -0400

    chore: add in missing image to default page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ngGetBeer';
-  secretActive=true;
-  clicks=0;
+  secretActive = false;
+  clicks = 0;
+
+  @HostListener('document:click', ['$event'])
+  documentClick(event: MouseEvent) {
+    this.clicks += 1;
+    if (this.clicks>2) {
+      this.clicks=0;
+      this.secretActive=true;
+    }
+  }
 }

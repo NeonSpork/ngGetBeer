0207e0c726d76f1e2a5f0e4f452d8db82ff20a37
neonSpork <leiknes@gmail.com>
2021-04-29 23:34:07 +0200

    reafactor: WIP gpio stuff

--- src/app/pages/beer/beer.component.ts
+++ src/app/pages/beer/beer.component.ts
@@ -1,8 +1,7 @@
-import { Component, Output, EventEmitter, AfterViewInit } from '@angular/core';
+import { Component, Output, EventEmitter, AfterViewInit, OnDestroy } from '@angular/core';
+import { interval } from 'rxjs';
+import { takeWhile } from "rxjs/operators";
 var rpio = require('rpio');
-import { Board, Thermometer } from 'johnny-five';
-var hx = require('@ataberkylmz/hx711');
-
 
 @Component({
   selector: 'app-beer',
@@ -10,49 +9,21 @@ var hx = require('@ataberkylmz/hx711');
   styleUrls: ['./beer.component.scss']
 })
 export class BeerComponent implements AfterViewInit {
-
-  constructor() { }
+  beerPin = 37;
+  constructor() {
+    rpio.init({mock: 'raspi-3'});
+  }
 
   ngAfterViewInit(): void {
+    rpio.open(this.beerPin, rpio.OUTPUT, rpio.LOW);
+  }
+
+  ngOnDestroy() {
+    rpio.exit();
   }
 
   temp = 0;
-  initializeTemp() {
-    const board = new Board();
-
-    board.on("ready", () => {
-      // This requires OneWire support using ConfigurableFirmata
-      const thermometer = new Thermometer({
-        controller: "DS18B20",
-        pin: 4
-      });
-
-      thermometer.on("change", () => {
-        const { celsius } = thermometer;
-        console.log("  celsius      : ", celsius);
-        this.temp = celsius;
-      });
-    });
-  }
-
   // Load sensor
-  clockpin: number = 2;
-  datapin: number = 3;
-  hx = new HX711(this.clockpin, this.datapin);
-
-  // pints: number = 99; // HACK placeholder
-  pints = setInterval(() => { this.calculatePints(); }, 60000);
-
-  calculatePints() {
-    this.hx.setOffset(8234508);
-    this.hx.setScale(-20.9993);
-    let grams: number = this.hx.getUnits();
-    let approxPints = (grams - 4250) * 0.002;
-    if (approxPints < 0) {
-      approxPints = 0;
-    }
-    return approxPints;
-  }
 
   @Output() addClick = new EventEmitter<number>();
 
@@ -61,8 +32,11 @@ export class BeerComponent implements AfterViewInit {
     this.addClick.emit(1);
   }
 
-  openBeer() {
-    this.temp += 1;
-  }
-
+  const beerValve = interval(10000)
+  .pipe(takeWhile(() => !stop))
+  .subscribe(() => {
+      openBeer() {
+        this.temp += 1;
+      }
+    });
 }

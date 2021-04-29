88e6826f831b666cb262afbfa9e5fe8b21d98188
neonSpork <leiknes@gmail.com>
2021-04-29 23:18:12 +0200

    feat: naive sensor service

---
+++ src/app/sensors.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { Board, Thermometer } from 'johnny-five';
+var HX711 = require('@ataberkylmz/hx711');
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SensorsService {
+
+  constructor(
+    private tempRead: number,
+    private pintsRead: number,
+    private hx: typeof HX711,
+  ) {
+    this.tempRead = 0;
+    this.pintsRead = 0;
+
+    // Temp read
+    const board = new Board();
+    board.on("ready", () => {
+      // This requires OneWire support using ConfigurableFirmata
+      const thermometer = new Thermometer({
+        controller: "DS18B20",
+        pin: 4
+      });
+      thermometer.on("change", () => {
+        const { celsius } = thermometer;
+        this.tempRead = celsius;
+      });
+    });
+
+    // Weight read
+    const clockpin: number = 2;
+    const datapin: number = 3;
+    hx = new HX711(clockpin, datapin);
+    hx.setOffset(8234508);
+    hx.setScale(-20.9993);
+  }
+
+  public getTemp() {
+    return this.tempRead;
+  }
+
+  public getPints() {
+    let grams: number = this.hx.getUnits();
+    this.pintsRead = (grams - 4250) * 0.002;
+    if (this.pintsRead < 0) {
+      this.pintsRead = 0;
+    }
+    return this.pintsRead;
+  }
+}

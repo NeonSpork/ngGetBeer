a59200eea80d40204710969c8fba12bd7856363f
neonSpork <leiknes@gmail.com>
2021-05-03 23:37:36 +0200

    fix: correct endpoint https

--- src/app/shared/flask-connector.service.ts
+++ src/app/shared/flask-connector.service.ts
@@ -7,7 +7,7 @@ import { catchError, map } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class FlaskConnectorService {
-  endpoint = 'http://localhost:5000/';
+  endpoint = 'http://localhost:5000/api/';
 
   constructor(private http: HttpClient) {}
 
